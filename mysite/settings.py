db8e52c287606c989ef76ee559802939a8fe0275
jadenfigger <jadenfigger@gmail.com>
2023-01-12 16:06:34 -0700

    commit on main

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'player_scraping',
     'bootstrap5',
+    'django_bootstrap_icons'
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'mysite.urls'
 
@@ -122,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+
 
 STATICFILES_DIRS  = [
     os.path.join(BASE_DIR, 'static'),
